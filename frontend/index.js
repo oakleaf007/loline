589680ff25724fa8980dfd95b46b5ecec28b9be0
OakLeaf007 <jjjbbbb736@gmail.com>
2025-11-08 11:21:57 +0530

    initial

--- frontend/index.js
+++ frontend/index.js
@@ -1,12 +1,44 @@
 const contactchat = document.querySelectorAll(".chat-contact");
 const chatbox= document.getElementById("chat-box");
 const welcome= document.getElementById("chat-intro");
-contactchat.forEach(e=>{
+
+
+
+const list = document.getElementById("list");
+const screenSize = window.matchMedia('(max-width:540px)');
+
+
+const btn = document.getElementById("back");
+
+if(btn){
+btn.addEventListener("click",async()=>{
+
+    chatbox.classList.remove("active");
+    list.style.display="flex";
+})
+
+}
+
+
+
+ contactchat.forEach(e=>{
 
 e.addEventListener("click",()=>{
 chatbox.classList.add("active");
+
 welcome.style.display="none";
 const name = e.dataset.name;
+
 document.getElementById("current-name").textContent=name;
+
+if(screenSize.matches){
+list.style.display="none";
+}else{
+    list.style.display="flex";
+}
 });
-})
+});
+
+
+
+
