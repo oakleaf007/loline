e4286ca567bfd4d06d997f7bf384e0f141aeaf5b
OakLeaf007 <jjjbbbb736@gmail.com>
2025-11-12 14:50:34 +0530

    ui update

--- frontend/loginmenu.js
+++ frontend/loginmenu.js
@@ -1,14 +1,18 @@
-const skip =document.getElementById("skipbtn");
+// const skip =document.getElementById("skipbtn");
 const login = document.getElementById("login-screen");
 
 
-skip.addEventListener("click", async()=>{
-document.getElementById("main").style.display="flex";
-login.style.display="none";
-})
-const skip2 =document.getElementById("skipbtn2");
-skip2.addEventListener("click", async()=>{
-document.getElementById("main").style.display="none";
-login.style.display="flex";
+// const skip2 =document.getElementById("skipbtn2");
+// skip2.addEventListener("click", async()=>{
+// document.getElementById("main").style.display="none";
+// login.style.display="flex";
 
+// })
+
+
+document.getElementById("form").addEventListener("submit", function(e){
+    e.preventDefault();
+
+    document.getElementById("main").style.display="flex";
+login.style.display="none";
 })
