d74c2a157c8a6820ba06458fce3e13f4dae81f25
OakLeaf007 <jjjbbbb736@gmail.com>
2025-12-12 20:05:15 +0530

    ui update

--- frontend/keepMeWake.js
+++ frontend/keepMeWake.js
@@ -16,3 +16,5 @@
 
 // }
 // KeepMeWake();
+
+// setInterval(KeepMeWake, 600000);
---
+++ frontend/loader.js
@@ -0,0 +1,4 @@
+window.addEventListener("load",()=>{
+    document.getElementById("dot-container").style.display="none";
+
+})
